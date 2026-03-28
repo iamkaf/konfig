7e46ec393088cb3dc9fbdb547aa26281605cfda1
Kaf <coffeechopdev@gmail.com>
2026-03-28 20:45:28 -0300

    Enable Forge for 26.1

--- 26.1/forge/src/main/java/com/iamkaf/konfig/forge/api/v1/KonfigForgeClientScreens.java
+++ 26.1/forge/src/main/java/com/iamkaf/konfig/forge/api/v1/KonfigForgeClientScreens.java
@@ -2,16 +2,17 @@ package com.iamkaf.konfig.forge.api.v1;
 
 import com.iamkaf.konfig.api.v1.KonfigClientScreens;
 import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.fml.ModList;
+import net.minecraftforge.fml.loading.LoadingModList;
 
 public final class KonfigForgeClientScreens {
     private KonfigForgeClientScreens() {
     }
 
     public static void register(String modId) {
-        String displayName = ModList.get()
-                .getModContainerById(modId)
-                .map(container -> container.getModInfo().getDisplayName())
+        String displayName = LoadingModList.getMods().stream()
+                .filter(info -> info.getModId().equals(modId))
+                .findFirst()
+                .map(info -> info.getDisplayName())
                 .orElse(modId);
         MinecraftForge.registerConfigScreen(parent -> KonfigClientScreens.create(modId, displayName, parent));
     }
